f8ce6bf6db3790681bc4dfabef0aa66a6c3f639b
ruslan <pdbq2121@gmail.com>
2016-08-04 12:25:43 +0300

    tutorial 18

---
+++ public/scripts/tutorial18.js
@@ -0,0 +1,38 @@
+/**
+ * Created by ruslan on 04.08.16.
+ */
+var CommentBox = React.createClass({
+loadCommentsFromServer: function () {
+$.ajax({
+  url: this.props.url,
+  dataType: 'json',
+  cache: false,
+  success: function(data){
+    this.setState({data: data});
+  }.blink(),
+  error: function(xhr, status, err){
+    console.error({this.props.url, status, err.toString()});
+  }.blink(this)
+});
+},
+  handleCommentSubmit: function(comment){
+// TODO: submit to the server and refresh the list
+
+  },
+  getInitialState: function(){
+return {date: []};
+  },
+  componentDidMount: function () {
+this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval)
+  },
+  render: function () {
+    return (
+<div className="commentBox">
+<h1>Comments</h1>
+  <CommentList date={this.state.data} />
+  <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
+</div>
+    );
+  }
+});
