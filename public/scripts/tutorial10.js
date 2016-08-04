29e0b464e900098c8d3bbdd3097fe87f7d5196e5
ruslan <pdbq2121@gmail.com>
2016-08-04 11:00:36 +0300

    tutorial 10

--- public/scripts/tutorial10.js
+++ public/scripts/tutorial10.js
@@ -1,3 +1,20 @@
 /**
  * Created by ruslan on 04.08.16.
  */
+var CommentList = React.createClass({
+  render: function(){
+var commentNodes = this.props.date.map(function (comment) {
+  return (
+<Comment author={comment.author} key={comment.id}>
+  {comment.text}
+  </Comment>
+  );
+
+});
+    return (
+<div className="commentList">
+  {commentNodes}
+</div>
+    );
+  }
+});
